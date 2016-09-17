cab7b8a3f7387a81793168f604cd1e9ff63bebc1
RDuuke <juuanduuke@gmail.com>
2016-09-16 23:45:18 -0500

    interfaz home item y contacto!

--- resource/views/layout/base.tpl.php
+++ resource/views/layout/base.tpl.php
@@ -13,44 +13,29 @@
 <body>
 <ul id="slide-out" class="side-nav">
     <li class="blue lighten-2">
-        <a href="<?= BASE_PUBLIC ?>/users/1" class="waves-effect"><span class="white-text"> <i class="material-icons small">person_pin</i>&nbsp; Mi perfil </span></a>
+        <a href="<?= BASE_PUBLIC ?>/users/1" class="waves-effect"><span class="white-text"> <i class="material-icons tiny">person_pin</i>&nbsp; Mi perfil </span></a>
     </li>
     <li class="blue lighten-2">
-        <a href="#!email" class="waves-effect"><span class="white-text"><i class="material-icons small">cloud</i>&nbsp; Mis Archivos</span></a>
+        <a href="#!email" class="waves-effect"><span class="white-text"><i class="material-icons tiny">cloud</i>&nbsp; Mis Archivos</span></a>
     </li>
     <li class="blue lighten-2">
-        <a href="#!" class="waves-effect"><span class="white-text"><i class="material-icons small">input</i>&nbsp; Cerrar sesión</a>
+        <a href="#!" class="waves-effect"><span class="white-text"><i class="material-icons tiny">input</i>&nbsp; Cerrar sesión</a>
     </li>
     <li><div class="divider"></div></li>
-    <li><a class="waves-effect" href="#!"><i class="material-icons small">video_library</i>&nbsp; Videos</a></li>
-    <li><a class="waves-effect" href="#!"><i class="material-icons small">perm_media</i>&nbsp; Imagenes</a></li>
-    <li><a class="waves-effect" href="#!"><i class="material-icons small">work</i>&nbsp; Documentos</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons tiny">video_library</i>&nbsp; Videos</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons tiny">perm_media</i>&nbsp; Imagenes</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons tiny">work</i>&nbsp; Documentos</a></li>
     <li><div class="divider"></div></li>
-    <li><a class="waves-effect" href="#!"><i class="material-icons small">perm_device_information
+    <li><a class="waves-effect" href="#!"><i class="material-icons tiny">perm_device_information
             </i>&nbsp;  Contacto</a></li>
 </ul>
 <div class="navbar-fixed">
     <nav class="blue lighten-3">
         <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-        <a href="#" class="brand-logo thin">Didactico repository</a>
+        <a href="#" class="brand-logo thin">Didactic repository</a>
     </nav>
 </div>
 <div class="container">
-    <br>
-    <div class="col s12 m4 l6 offset-s4 offset-l3 center-align">
-        <div class="row">
-            <div class="input-field col s12">
-                <select>
-                    <option value="" disabled selected>Choose your option</option>
-                    <option value="1">Option 1</option>
-                    <option value="2">Option 2</option>
-                    <option value="3">Option 3</option>
-                </select>
-                <label>Type:</label>
-            </div>
-        </div>
-    </div>
-</div>
 <?= $this->section('content')?>
 </div>
 <?php script('js/jquery.min.js')?>
