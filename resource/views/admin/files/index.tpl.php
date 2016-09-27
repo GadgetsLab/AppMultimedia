e5114794dbf0ca4c032a8c6917377740b33f4d3e
Gustavo Adolfo Muñoz Reyes <Gustavo Adolfo Muoz Reyes>
2016-09-27 09:31:10 -0500

    Distinccion entre archivos videos e imagenes

--- resource/views/admin/files/index.tpl.php
+++ resource/views/admin/files/index.tpl.php
@@ -10,15 +10,35 @@
 <div class="row"> 
     <?php foreach($files as $file): ?>
         <div class="col s12 m6 l6">
-            <div class="panel hoverable"> 
-                <div class="panel-title  blue darken-1"> 
-                    <h4><?= $file->title ?></h4>
-                </div>
-                <div class="panel-body"> 
-                    <p>Description: <?= $this->e($file->description) ?></p>
-                    <p>Tipo: <?= $this->e($file->type)?></p>
-                    <?= route('admin/files/'.$file->id, 'Ver', null, ['class' => 'btn blue darken-1']) ?>
-                </div>
+            <div class="panel hoverable">
+                <?php if ($file->type == 'video'): ?>
+                    <div class="panel-title  blue darken-1"> 
+                        <h4><?= $file->title ?></h4>
+                    </div>
+                    <div class="panel-body"> 
+                        <p>Description: <?= $this->e($file->description) ?></p>
+                        <p>Tipo: <?= $this->e($file->type)?></p>
+                        <?= route('admin/files/'.$file->id, 'Ver', null, ['class' => 'btn blue darken-1']) ?>
+                    </div>
+                <?php elseif ($file->type == 'image'): ?>
+                    <div class="panel-title  green darken-1"> 
+                        <h4><?= $file->title ?></h4>
+                    </div>
+                    <div class="panel-body"> 
+                        <p>Description: <?= $this->e($file->description) ?></p>
+                        <p>Tipo: <?= $this->e($file->type)?></p>
+                        <?= route('admin/files/'.$file->id, 'Ver', null, ['class' => 'btn green darken-1']) ?>
+                    </div>
+                <?php elseif ($file->type == 'document'): ?>
+                    <div class="panel-title  red darken-1"> 
+                        <h4><?= $file->title ?></h4>
+                    </div>
+                    <div class="panel-body"> 
+                        <p>Description: <?= $this->e($file->description) ?></p>
+                        <p>Tipo: <?= $this->e($file->type)?></p>
+                        <?= route('admin/files/'.$file->id, 'Ver', null, ['class' => 'btn red darken-1']) ?>
+                    </div>
+                <?php endif ?>
             </div>
         </div>
     <?php endforeach?>
