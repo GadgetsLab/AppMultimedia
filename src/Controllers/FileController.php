469ef95dcea997ddc6344dca30bef13bbb3a796d
daesca <reickchozo@gmail.com>
2016-10-05 23:08:23 -0500

    Commit inutil

--- src/Controllers/FileController.php
+++ src/Controllers/FileController.php
@@ -3,7 +3,7 @@
 namespace RDuuke\Newbie\Controllers;
 
     use RDuuke\Newbie\Controllers\Base\FileBaseController;
-use RDuuke\Newbie\Files;
+    use RDuuke\Newbie\Files;
 
 class FileController extends FileBaseController
 {
